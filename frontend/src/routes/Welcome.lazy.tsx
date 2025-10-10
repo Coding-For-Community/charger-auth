8f664d16422bee29df3288be73fb0da06628e9af
Daniel1464 <Daniel_Chen@caryacademy.org>
2025-10-09 20:49:27 -0400

    Add uncommited changes

---
+++ frontend/src/routes/Welcome.lazy.tsx
@@ -0,0 +1,21 @@
+import { Text, Title } from "@mantine/core";
+import { createLazyFileRoute } from "@tanstack/react-router";
+
+export const Route = createLazyFileRoute("/Welcome")({
+  component: WelcomePage,
+});
+
+function WelcomePage() {
+  return (
+    <>
+      <Title>Welcome to ChargerAuth!</Title>
+      <Text>
+        You may have noticed that the various free period sign-in kiosks
+        around campus now have QR codes that you can scan. They all use the ChargerAuth system!
+      </Text>
+      <Text>
+        You can scan the QR code with just your phone, but 
+      </Text>
+    </>
+  )
+}
